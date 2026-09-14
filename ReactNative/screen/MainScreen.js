cccd211dd577a254934d28a8305a9328fb97b026
donesbajrami360-hash <donesbajrami360@gmail.com>
2026-09-14 18:28:58 +0200

    w

---
+++ ReactNative/screen/MainScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Text , StyleSheet , View} from "react-native";
+
+const MainScreen =()=> {
+    return(<View style={styles.test}>
+        <Text style={styles.textstyle}>
+            This is a main screen
+        </Text>
+    </View>);
+}
+
+const styles = StyleSheet.create({
+    test: {
+        flex: 1,
+        backgroundColor: "lightblue",
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+    textStyle:{
+        fontSize: 30
+    }
+})
+
+export default MainScreen;
